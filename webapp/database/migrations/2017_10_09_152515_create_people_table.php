b2fb2421233a521d0e3816b332aa154f33b9ed05
Bruno <fernando23296@gmail.com>
2017-10-09 17:41:05 -0400

    user.php is now in a new folder (models) and the creation of the first table

---
+++ webapp/database/migrations/2017_10_09_152515_create_people_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->string('idpersona',15)->primary();
+            $table->string('tipo_persona',20);
+            $table->string('nombre',100);
+            $table->string('tipo_documento',20);
+            $table->string('num_documento',15);
+            $table->string('direccion',70);
+            $table->string('telefono',15);
+            $table->string('email',50);
+            $table->integer('user')->unsigned();
+            $table->foreign('user')
+                  ->references('id')
+                  ->on('users');
+
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
