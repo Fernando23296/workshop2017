1826887313293b25bfb910201f38fa3a1527bab2
Bruno <fernando23296@gmail.com>
2017-10-09 01:28:05 -0400

    changing values for db connection

--- webapp/config/database.php
+++ webapp/config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,9 +58,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'taller_db'),
+            'username' => env('DB_USERNAME', 'posgres'),
+            'password' => env('DB_PASSWORD', 'Ferdinand1996'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
