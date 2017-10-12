2fa3852e02ae445434ff66fb08da57ddfb422c2a
Bruno <fernando23296@gmail.com>
2017-10-12 00:04:45 -0400

    basic interface with css

--- webapp/resources/views/store/partials/footer.blade.php
+++ webapp/resources/views/store/partials/footer.blade.php
@@ -1,1 +1,30 @@
-footer.blade.php
+<footer class="container-fluid">
+
+	<div class="row">
+		<div class="col-md-4">
+		<h3>tienda</h3>
+		<p>descripion basica de la tienda hahah</p>
+	</div>
+	<div class="col-md-4">
+		<h3>developed by erick and bruno</h3>
+		<div class="author-info">
+			<img src="">
+			<p>asdas das das d as d as das d a das d as d asda s das d asd asd</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<h3>siguenos</h3>
+		<ul class="redes">
+			<li>
+			<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>	
+			</li>
+			<li>
+			<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>	
+			</li>
+		</ul>
+		<h3>Escribenos</h3>
+		<i class="fa fa-at"></i><a href="#">tucus@gmail.com</a>
+		</div>
+		</div>
+		</div>
+
--- webapp/resources/views/store/template.blade.php
+++ webapp/resources/views/store/template.blade.php
@@ -4,6 +4,7 @@
 		<title>@yield('title','tucus')</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css" rel="stylesheet">
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="{{ asset('css/main.css')}}">
 	</head>
 
 	<body>
