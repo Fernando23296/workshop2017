02513d05f5473d8a4843a1dfbfe2946ff59c33a8
Bruno <fernando23296@gmail.com>
2017-11-07 16:55:57 -0400

    adding details in the admin page

--- resources/views/components/who.blade.php
+++ resources/views/components/who.blade.php
@@ -1,10 +1,17 @@
+<title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 @if (Auth::guard('web')->check())
 	<p class="text-sucess">
 		Has ingresado como <strong> USUARIO </strong>
 	</p>
 @else
 	<p class="text-danger">
-		Has salido como <strong> USUARIO</strong>
+		No has ingresado <strong> USUARIO</strong>
 	</p>
 
 @endif
@@ -13,9 +20,25 @@
 	<p class="text-sucess">
 		Has ingresado como <strong> ADMINISTRADOR </strong>
 	</p>
+
+  
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand">Menu Administrador</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="{{ url('/store/products') }}"><p>Añadir Productos  <span class="glyphicon glyphicon-plus"></span></p></a></li>
+      <li><a href="#"><p>Añadir Usuarios  <span class="glyphicon glyphicon-user"></span></p></a></li>
+      <li><a href="#"><p>Añadir Productos  <span class="glyphicon glyphicon-plus"></span></p></a></li>
+    </ul>
+  </div>
+</nav>
+                  
 @else
 	<p class="text-danger">
-		Has salido como <strong> ADMINISTRADOR</strong>
+		No has ingresado <strong> ADMINISTRADOR</strong>
 	</p>
+
 
 @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,21 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">-->
+         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+ 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 
         <!-- Styles -->
         <style>
@@ -65,6 +73,7 @@
         </style>
     </head>
     <body>
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
