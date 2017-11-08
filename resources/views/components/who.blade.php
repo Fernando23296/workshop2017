5f5ddae6c0098c64d3801cdcb63bb4b633449fe9
Bruno <fernando23296@gmail.com>
2017-11-08 08:23:43 -0400

    details in blades

--- resources/views/components/who.blade.php
+++ resources/views/components/who.blade.php
@@ -1,44 +1,38 @@
-<title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+<ul class="list-group">
 @if (Auth::guard('web')->check())
-	<p class="text-sucess">
+	  <li class="list-group-item"><p class="text-sucess">
 		Has ingresado como <strong> USUARIO </strong>
-	</p>
+	</p></li>
 @else
-	<p class="text-danger">
-		No has ingresado <strong> USUARIO</strong>
-	</p>
+	  <li class="list-group-item"><p class="text-danger">
+		No has ingresado como <strong> USUARIO</strong>
+	</p></li>
 
 @endif
 
 @if (Auth::guard('admin')->check())
-	<p class="text-sucess">
+	  <li class="list-group-item"><p class="text-sucess">
 		Has ingresado como <strong> ADMINISTRADOR </strong>
-	</p>
+	</p></li>
 
-  
+</ul>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand">Menu Administrador</a>
     </div>
     <ul class="nav navbar-nav">
-      <li><a href="{{ url('/store/products') }}"><p>Añadir Productos  <span class="glyphicon glyphicon-plus"></span></p></a></li>
-      <li><a href="#"><p>Añadir Usuarios  <span class="glyphicon glyphicon-user"></span></p></a></li>
-      <li><a href="#"><p>Añadir Productos  <span class="glyphicon glyphicon-plus"></span></p></a></li>
+      <li><a href="{{ url('/store/products') }}"><strong>Añadir Productos</strong></a></li>
+      <li><a href="{{ url('/register') }}"><strong>Añadir Usuarios</strong></a></li>
     </ul>
   </div>
 </nav>
                   
 @else
-	<p class="text-danger">
+	<li class="list-group-item"><p class="text-danger">
 		No has ingresado <strong> ADMINISTRADOR</strong>
-	</p>
+	</p></li>
 
 
 @endif
