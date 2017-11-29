ef1957b1a54733f2d45f498a081e070953178889
Bruno <fernando23296@gmail.com>
2017-11-29 07:39:30 -0400

    chart routes

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Charts;
 use App\Models\Product;
+use App\Models\User;
+use App\Models\Store;
 class chartController extends Controller
 {
     //
@@ -12,17 +14,34 @@ class chartController extends Controller
     {
        
 
-    $chart = Charts::database(Product::all(), 'bar', 'google')
+    $chart = Charts::database(Product::all(), 'donut', 'google')
     ->title('Tipos de Productos en Stock')
     ->elementLabel("Total")
     ->dimensions(1000, 500)
     ->responsive(false)	
     ->groupBy('stock');
 
-    	return view('chart', ['chart' => $chart]);
+
+
     
+    $bar = Charts::database(Product::all(), 'bar', 'google')
+    ->title('Tipos de Productos')
+    ->elementLabel("Total")
+    ->dimensions(1000, 500)
+    ->responsive(false) 
+    ->groupBy('tipo');
+
+    $precio = Charts::database(Product::all(), 'pie', 'google')
+    ->title('Precios')
+    ->elementLabel("precio")
+    ->dimensions(1000, 500)
+    ->responsive(false) 
+    ->groupBy('precio');
+
+   
+
 	
-    
+      return view('chart',['chart' => $chart, 'bar' => $bar, 'precio' => $precio,]);
 
 
     }
--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -3,23 +3,71 @@
 @section('content')
    	
     <body>
-        <!-- Main Application (Can be VueJS or other JS framework) -->
-        <li><div class="app">
-            <center>
-                {!! $chart->html() !!}
 
-            </center>
-        </div></li>
-        <div class="app">
+            
+           
+
+    <table>
+
+        <tr align="center">
+
+            <div class="row">
+                <div class="panel">
+                    <a href="{{url('admin/home')}}" class="btn btn-warning btn-block btn-home-admin">VOLVER PANEL ADMIN</a>
+                    <i class="fa fa-user icon-home"></i>
+                    <a href="{{ route('admin.login') }}" class="btn btn-warning btn-block btn-home-admin">BIENVENIDO A LA PAGINA DE REPORTES</a>
+                   <center>
+                   <ul><H3>Productos</H3>
+                       <li><H4><a href="#primero">Reporte segun STOCK</a></H4></li>
+                       <li><H4><a href="#segundo">Reporte segun TIPOS</a></H4></li>
+
+                       <li><H4><a href="#tercero">Reporte segun PRECIOS</a></H4></li>
+                   </ul>
+               </center>
+      
+            </div>
+        </tr>
+        <a name="primero"><tr>
+             <hr>
+            <div class="app">
             <center>
-                {!! $chart->html() !!}
+                
                 {!! $chart->html() !!}
 
             </center>
         </div>
-        <!-- End Of Main Application -->
-        {!! Charts::scripts() !!}
+        <hr>
+        </tr></a>
+        <a name="segundo"><tr>
+            <div class="app">
+            <center>
+                
+                {!! $bar->html() !!}
+
+            </center>
+        </div>
+         <hr>
+
+        </tr></a>
+
+         <a name="tercero"><tr>
+            <div class="app">
+            <center>
+                
+                {!! $precio->html() !!}
+          
+
+            </center>
+        </div>
+        <hr>
+        </tr></a>
+
+    </table>
+
+    {!! Charts::scripts() !!}
         {!! $chart->script() !!}
 
+        {!! $bar->script() !!}
+        {!! $precio->script() !!}
     </body>
 @stop
